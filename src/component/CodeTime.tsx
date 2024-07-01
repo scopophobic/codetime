684f1226ae64fd26ba9bb67c3abbed4e17a61c1f
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-07-02 01:33:56 +0530

    removed API KEY
    YOU DUMB SHIT

--- src/component/CodeTime.tsx
+++ src/component/CodeTime.tsx
@@ -8,7 +8,7 @@ const CodeTime = () => {
   const [error, setError] = useState(null);
 
   // Fetch API key from environment variables or a secure storage mechanism
-  const API_KEY = "AIzaSyDg78AZkN9WFTm6aABosaZiwK03OioxQwA";
+  const API_KEY = "";
   console.log(API_KEY);
   const genAI = new GoogleGenerativeAI(API_KEY);
 
