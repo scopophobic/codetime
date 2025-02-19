3496b07bd9375a5d716cb746faf8d3480d9b4239
scopophobic <adithyanmadhu1234@hmail.com>
2025-02-19 19:32:00 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import CodeComplexityAnalyzer from './component/CodeComplexityAnalyzer'
 import Navbar from './component/navbar'
 import CodeTime from './component/CodeTime'
 import Foot from './component/Foot'
